404fff6dae017bfdfb24cb7ba8c038b2ce338278
sion0310 <92357569+sion0310@users.noreply.github.com>
2021-11-25 18:01:57 +0900

    1125_학원

--- 1124_RPG/Assets/Scripts/CSVReader.cs
+++ 1124_RPG/Assets/Scripts/CSVReader.cs
@@ -15,25 +15,36 @@ public class CSVReader
         var list = new List<Dictionary<string, object>>();
         TextAsset data = Resources.Load(file) as TextAsset;
 
+        //data.text를 행이 바뀔때마다 잘라서 lines에 넣는다
         var lines = Regex.Split(data.text, LINE_SPLIT_RE);
 
+        //lines의 길이가 1이하이면 그냥 리스트로
         if (lines.Length <= 1) return list;
 
+        //lines 첫 행의 칸이 바뀔때마다 잘라서 header에 넣는다
         var header = Regex.Split(lines[0], SPLIT_RE);
+
+        //header를 제외한 나머지 값들 정리
         for (var i = 1; i < lines.Length; i++)
         {
-
+            //lines 1행부터 칸이 바뀔때마다 잘라서 values에 넣는다
             var values = Regex.Split(lines[i], SPLIT_RE);
-            if (values.Length == 0 || values[0] == "") continue;
 
+            //각행의 길이가 0이거나 값이 없을때는 다음단계로
+            if (values.Length == 0 || values[0] == "") continue;
+            
             var entry = new Dictionary<string, object>();
+
+            //각 값들을 항목별로 넣어주는과정
             for (var j = 0; j < header.Length && j < values.Length; j++)
             {
+                
                 string value = values[j];
+                //대충 \" 부터 \" 까지 덩어리로 잘라서 넣는다는거 같긴함(replace는 문자를 바꿔주는것)
                 value = value.TrimStart(TRIM_CHARS).TrimEnd(TRIM_CHARS).Replace("\\", "");
                 object finalvalue = value;
                 int n;
-                float f;
+                float f;//각 값들을 자료형에 맞게 바꿔주는 과정
                 if (int.TryParse(value, out n))
                 {
                     finalvalue = n;
@@ -44,6 +55,8 @@ public class CSVReader
                 }
                 entry[header[j]] = finalvalue;
             }
+
+            //분류가 끝난애들 리스트에 넣어주는것
             list.Add(entry);
         }
         return list;
--- 1124_RPG/Assets/Scripts/CSV_ex.cs
+++ 1124_RPG/Assets/Scripts/CSV_ex.cs
@@ -10,12 +10,15 @@ public class CSV_ex : MonoBehaviour
 
         List<Dictionary<string, object>> data = CSVReader.Read("example");
 
+            string[] scripts = ((string)data[0]["Dialogue"]).Split('$');
         for (var i = 0; i < data.Count; i++)
         {
-            print(data[i]["ID"] + " " +
-                  data[i]["인물이름"] + " " +
-                  data[i]["대사"] + " " +
-                  data[i]["이벤트번호"]);
+
+            print(data[i]["Index"] + " " +
+                  data[i]["Name"] + " " +
+                  data[i]["Dialogue"] + " " +
+                  data[i]["EventNum"] + " " +
+                  data[i]["SkipLine"]);
         }
 
     }
